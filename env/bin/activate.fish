c260ec59e0fdb09ab7a46de78fb5e377ccefeadb
evlyn57 <evlyn57@gmail.com>
2016-09-18 11:47:05 -0700

    deleted env and requirements

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/lorarose/floobits/hiddenGems/env"
+set -gx VIRTUAL_ENV "/Users/Evlyn/src/hiddenGems2/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
