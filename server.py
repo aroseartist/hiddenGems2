d731ac695495ee29dd8c9e15bb562009c49ba0d0
Lora Rose <aroseartist@gmail.com>
2016-09-18 10:56:53 -0700

    prettified js for front end

--- server.py
+++ server.py
@@ -32,9 +32,7 @@ app.jinja_env.auto_reload = True
 # Routes app index page to homepage
 def index():
     """Homepage."""
-    return render_template("base.html", input_box_name_here=None)
-
-
+    return render_template("contact.html", input_box_name_here=None)
 
 
 
