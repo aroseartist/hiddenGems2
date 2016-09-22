a49e7021716af47739a569a1c0ae69d483d9c9ce
Lora Rose <aroseartist@gmail.com>
2016-09-22 16:10:43 -0700

    adding jinja

--- server.py
+++ server.py
@@ -1,26 +1,21 @@
 """Server file for Hidden Gems."""
 
 # Import necessary modules, etc.
-
 # Utilize Jinja for HTML templates
 from jinja2 import StrictUndefined
 # Utilize Flask libraries
 from flask import Flask, render_template, request, flash, redirect, session, jsonify
+#Use toolbar for debugging
+# from flask_debugtoolbar import DebugToolbarExtension
+from model import connect_to_db, db, Places
 # Access local env variables
 import os
-import sys
-
 import psycopg2
-
-from model import connect_to_db, db, Places
-# Use toolbar for debugging
-# from flask_debugtoolbar import DebugToolbarExtension
-
+import sys
+# Tells Flask where to find files / dirs
 app = Flask(__name__, static_url_path='/static')
-
 # Set a secret key to enable the flask session cookies and the debug toolbar
 app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET_KEY", "seKriTz")
-
 # Ensures undefined variables in jinja raise an error
 app.jinja_env.undefined = StrictUndefined
 # allows html to reload without restarting server
@@ -47,17 +42,17 @@ if __name__ == "__main__":
 
     connect_to_db(app)
     db.create_all(app=app)
-    # Set debug=True in order to invoke the DebugToolbarExtension
+    #Set debug=True in order to invoke the DebugToolbarExtension
     # app.debug = True
 
     # app.config['TRAP_HTTP_EXCEPTIONS'] = True
     # app.config['Testing'] = True
-    # Use of debug toolbar
+    #Use of debug toolbar
     # DebugToolbarExtension(app)
 
     # Run app locally
     app.run(host='0.0.0.0')
 
-    #  Run app via Heroku
+    #Run app via Heroku
     # PORT = int(os.environ.get("PORT", 5000))
     # app.run(host="0.0.0.0", port=PORT)
