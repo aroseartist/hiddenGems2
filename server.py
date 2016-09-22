079da11d91da1e472609e17c8cbce69326a31957
Lora Rose <aroseartist@gmail.com>
2016-09-22 15:34:32 -0700

    adding db queries and connectivity to server

--- server.py
+++ server.py
@@ -8,18 +8,21 @@ import jinja2
 import os
 import sys
 
+from model import connect_to_db, db, Places
 # Utilize Flask libraries
 from flask import (Flask, render_template, redirect, request, flash, jsonify)
+from flask_sqlalchemy import SQLAlchemy
 # Use toolbar for debugging
 # from flask_debugtoolbar import DebugToolbarExtension
 from jinja2 import StrictUndefined
 from sys import argv
 
-
+db = SQLAlchemy()
 app = Flask(__name__, static_url_path='/static')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-# Required to use Flask sessions and debug toolbar
-app.secret_key = 'FLASK_SECRET_KEY'
+# Set a secret key to enable the flask session cookies and the debug toolbar
+app.config['SECRET_KEY'] = os.environ.get("FLASK_SECRET_KEY", "seKriTz")
+
 
 # Ensures undefined variables in jinja raise an error
 app.jinja_env.undefined = StrictUndefined
@@ -30,20 +33,33 @@ app.jinja_env.auto_reload = True
 ###################### Core Routes ###########################
 
 
+def connect_to_db(app):
+    """Connect to database."""
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///places'
+    db.app = app
+    db.init_app(app)
+
 
 @app.route('/')
 # Routes app index page to homepage
 def index():
     """Homepage."""
-    return render_template("index.html", input_box_name_here=None)
 
+    places = db.session.execute("SELECT * FROM places").fetchall()
+
+    return render_template("index.html", places=places)
 
 
 ################### Helper Functions #######################
 
+
 # Listening or requests
 if __name__ == "__main__":
     
+    connect_to_db(app)
+    db.create_all(app=app)
+
     # Set debug=True in order to invoke the DebugToolbarExtension
     # app.debug = True
 
@@ -51,13 +67,11 @@ if __name__ == "__main__":
     # app.config['Testing'] = True
     # Use of debug toolbar
     # DebugToolbarExtension(app)
-   
-    # connect_to_db(app)
 
     # Run app locally
     app.run(host='0.0.0.0')
     
-    #  Run app via Heroku
+    # Run app via Heroku
     # PORT = int(os.environ.get("PORT", 5000))
     # app.run(host="0.0.0.0", port=PORT)
 
